4af2eacd345756f3c839e5bc1bbd3c31d8e56030
kiri-1 <belenjesuslp@gmail.com>
2023-11-30 23:38:11 -0300

    finalizando crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   titulo: String = 'Angular Crud';
+  msg: string = '';
 
   empleados = [
-    { 'nombre': 'Soria', ocupacion: 'Robar kills' },
-    { 'nombre': 'Colazo', ocupacion: 'Hacker' },
-    { 'nombre': 'Alvarez', ocupacion: 'Ver furros' }
+    { 'nombre': 'Soria', ocupacion: 'Robar kills', email: 'elsuciodan@gmail.com' },
+    { 'nombre': 'Colazo', ocupacion: 'Hacker', email: 'elbarto@gmail.com' },
+    { 'nombre': 'Alvarez', ocupacion: 'Ver furros', email: 'elfurros@gmail.com' }
   ];
 
   model: any = {};
+  model2: any = {};
+  ocultarUpdate:boolean=true;
 
   /*  agregar */
   addEmpleado(): void {
-
-
+    /*   inserta datos en el arreglo */
+    this.empleados.push(this.model);
+    this.msg='campo agregado';
   }
   /*   borrar */
-  deleteEmpleado(): void {
-
+  deleteEmpleado(i: number): void {
+    var answer = confirm('Seguro de eliminar?');
+    if (answer) {
+      this.empleados.splice(i, 1);
+      this.msg='campo eliminado';
+    }
   }
   /*   editar */
-  editEmpleado(): void {
-
+  myValue: any;
+  editEmpleado(i: number): void {
+    this.ocultarUpdate = false;
+    this.model2.nombre = this.empleados[i].nombre;
+    this.model2.ocupacion = this.empleados[i].ocupacion;
+    this.model2.email = this.empleados[i].email;
+    this.myValue = i;
   }
   /*   actualizar */
   updateEmpleado(): void {
-
+    let i = this.myValue;
+    for (let j = 0; j < this.empleados.length; j++) {
+      if (i == j) {
+        this.empleados[i] = this.model2;
+        this.msg ='campo actualizado';
+        this.model2 = {};
+      }
+    }
   }
-
-
+  closeAlert(){
+    this.msg ='';
+  }
 }
